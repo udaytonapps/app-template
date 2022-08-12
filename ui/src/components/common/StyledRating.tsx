d6e05265d1594f807c3d66af89c54426a31fad6d
Andrew Niekamp <aniekamp1@udayton.edu>
2022-08-12 16:16:18 -0400

    Add common styled rating component (smileys)

---
+++ ui/src/components/common/StyledRating.tsx
@@ -0,0 +1,68 @@
+// https://mui.com/material-ui/react-rating/
+
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Rating, { IconContainerProps } from "@mui/material/Rating";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
+import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+
+const StyledRating = styled(Rating)(({ theme }) => ({
+  "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
+    color: theme.palette.action.disabled,
+  },
+}));
+
+const customIcons: {
+  [index: string]: {
+    icon: React.ReactElement;
+    label: string;
+  };
+} = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon color="error" />,
+    label: "Very Dissatisfied",
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon color="error" />,
+    label: "Dissatisfied",
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon color="warning" />,
+    label: "Neutral",
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon color="success" />,
+    label: "Satisfied",
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon color="success" />,
+    label: "Very Satisfied",
+  },
+};
+
+function IconContainer(props: IconContainerProps) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+interface RadioGroupRatingProps {
+  onChange: any;
+  value: any;
+}
+
+export default function RadioGroupRating(props: RadioGroupRatingProps) {
+  const { value, onChange } = props;
+  return (
+    <StyledRating
+      onChange={onChange}
+      name="highlight-selected-only"
+      value={value}
+      IconContainerComponent={IconContainer}
+      getLabelText={(value: number) => customIcons[value].label}
+      highlightSelectedOnly
+    />
+  );
+}
